72c712ddc0ab492c7c177c21f13d85ec92043704
dericwu <deric.wv18@outlook.com>
2020-06-01 19:32:53 +0100

    SG01 Create C++ Class
    Create an Objective Class and create a Blueprint based off of it

---
+++ StealthGameBase/Intermediate/Build/Win64/FPSGameEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/DW/Documents/GitHub/StealthGame/StealthGameBase/Intermediate/Build/Win64/FPSGameEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ StealthGameBase/Intermediate/Build/Win64/UE4Editor/Inc/FPSGame/FPSObjectiveActor.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FPSGAME_FPSObjectiveActor_generated_h
+#error "FPSObjectiveActor.generated.h already included, missing '#pragma once' in FPSObjectiveActor.h"
+#endif
+#define FPSGAME_FPSObjectiveActor_generated_h
+
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_SPARSE_DATA
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_RPC_WRAPPERS
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAFPSObjectiveActor(); \
+	friend struct Z_Construct_UClass_AFPSObjectiveActor_Statics; \
+public: \
+	DECLARE_CLASS(AFPSObjectiveActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/FPSGame"), NO_API) \
+	DECLARE_SERIALIZER(AFPSObjectiveActor)
+
+
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAFPSObjectiveActor(); \
+	friend struct Z_Construct_UClass_AFPSObjectiveActor_Statics; \
+public: \
+	DECLARE_CLASS(AFPSObjectiveActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/FPSGame"), NO_API) \
+	DECLARE_SERIALIZER(AFPSObjectiveActor)
+
+
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFPSObjectiveActor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFPSObjectiveActor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFPSObjectiveActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFPSObjectiveActor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFPSObjectiveActor(AFPSObjectiveActor&&); \
+	NO_API AFPSObjectiveActor(const AFPSObjectiveActor&); \
+public:
+
+
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFPSObjectiveActor(AFPSObjectiveActor&&); \
+	NO_API AFPSObjectiveActor(const AFPSObjectiveActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFPSObjectiveActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFPSObjectiveActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AFPSObjectiveActor)
+
+
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_PRIVATE_PROPERTY_OFFSET
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_9_PROLOG
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_SPARSE_DATA \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_RPC_WRAPPERS \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_INCLASS \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_SPARSE_DATA \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_INCLASS_NO_PURE_DECLS \
+	StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FPSGAME_API UClass* StaticClass<class AFPSObjectiveActor>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID StealthGameBase_Source_FPSGame_Public_FPSObjectiveActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
